c07530a7582b29f1e21538d3c21f60832fc7e672
Natdanai Pinaves <jgogo012@gmail.com>
2024-04-27 05:40:07 +0700

    Add: Index Page

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+header("Content-Type: application/json");
+$json = [
+    "status" => 200,
+    "message" => "Welcome to SAKU API",
+    "data" => [
+        "name" => "SAKU API",
+        "description" => "API for SAKU System",
+        "version" => "1.0.0",
+        "author" => "Kasetsart University",
+        "developer" => [
+            "name" => "SAKU Team",
+            "team" => [
+                "leader" => "Laddawan Wachiramekakun",
+                "members" => [
+                    "Teerut Sitthongdee",
+                    "Natdanai Pinaves",
+                    "Weeranut Chayakun",
+                    "Kittikun Boontoyut",
+                    "Krit Poka",
+                    "Thatpong Wongchaitha",
+                    "Chawit Phiphobsophonchai"
+                ]
+            ]
+        ],
+        "powered_by" => [
+            "Office of Computer Service, KU",
+            "Technology Club of Kasetsart University",
+            "Activity Team Student Development Division, KU",
+        ],
+    ]
+];
+echo json_encode($json, JSON_PRETTY_PRINT);
