103874f17d6d1d6ef459e5cce31888d912d53780
Natdanai Pinaves <jgogo012@gmail.com>
2024-05-15 03:59:52 +0700

    Edit: Allow CROS

--- components/header.php
+++ components/header.php
@@ -1,3 +1,10 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+?>
+
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <title>ระบบอนุมัติโครงการออนไลน์ - กองพัฒนานิสิต</title>
