c0eaca880c0a7513374ae55f692a38a96fc8adac
Natdanai Pinaves <jgogo012@gmail.com>
2024-05-15 14:10:15 +0700

    Edit: CORS

--- kusd1.php
+++ kusd1.php
@@ -4,6 +4,12 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header('Access-Contol-Allow-Credentials: true');
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require("connection.php");
 require("utils/function.php");
